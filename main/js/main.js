e5e69ab6724f16f0cedbd5e5880428054aadab98
KaluginaMarina <marin.kalugin@gmail.com>
2020-10-24 23:34:08 +0300

    add npm and orbitControl

--- main/js/main.js
+++ main/js/main.js
@@ -1,30 +1,43 @@
+import OrbitControls from "orbit-controls-es6";
+import THREE from  "three.js";
+
+var scene;
+var camera;
+var renderer;
+var controls;
+
 window.onload = function () {
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer();
+
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    animate();
+
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     document.body.appendChild(renderer.domElement);
 
-    renderCube(scene, camera, renderer)
+    renderCube()
 }
 
-function renderCube(scene, camera, renderer){
+function animate() {
+    requestAnimationFrame( animate );
+    controls.update();
+    renderer.render( scene, camera );
+}
 
-    const geometry = new THREE.BoxGeometry( 10, 10, 10);
-    const material = new THREE.MeshBasicMaterial( { color: 0x123456 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+function renderCube() {
+
+    const geometry = new THREE.BoxGeometry(10, 10, 10);
+    const material = new THREE.MeshBasicMaterial({color: 0x123456});
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
 
     camera.position.z = 25;
 
-    function render() {
-        requestAnimationFrame( render );
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        renderer.render( scene, camera );
-    }
-    render();
+    renderer.render(scene, camera);
 }
