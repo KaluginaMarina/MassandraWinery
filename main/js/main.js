3be4866b4a85d3856bd53405008f30c2aa030915
KaluginaMarina <marin.kalugin@gmail.com>
2020-10-24 19:10:05 +0300

    set canvas size

--- main/js/main.js
+++ main/js/main.js
@@ -1,4 +1,15 @@
-var canvas = document.getElementById('my_canvas');
-var gl = canvas.getContext('experimental-webgl');
-gl.clearColor(0.9,0.9,0.8,1);
-gl.clear(gl.COLOR_BUFFER_BIT);
+window.onload = function main() {
+    const canvas = document.getElementById('canvas');
+    const gl = canvas.getContext('webgl');
+    const ratio = window.devicePixelRatio || 1;
+    const width = innerWidth;
+    const height = innerHeight;
+    gl.canvas.width = width * ratio;
+    gl.canvas.height = height * ratio;
+    gl.canvas.style.width = width + 'px';
+    gl.canvas.style.height = height + 'px';
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    gl.clearColor(0.9, 0.9, 0.8, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
