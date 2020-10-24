782555202b6acbdd5dec76e028f378c009565504
KaluginaMarina <marin.kalugin@gmail.com>
2020-10-24 20:10:16 +0300

    render cube

--- main/js/main.js
+++ main/js/main.js
@@ -1,15 +1,30 @@
-window.onload = function main() {
-    const canvas = document.getElementById('canvas');
-    const gl = canvas.getContext('webgl');
-    const ratio = window.devicePixelRatio || 1;
-    const width = innerWidth;
-    const height = innerHeight;
-    gl.canvas.width = width * ratio;
-    gl.canvas.height = height * ratio;
-    gl.canvas.style.width = width + 'px';
-    gl.canvas.style.height = height + 'px';
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+window.onload = function () {
 
-    gl.clearColor(0.9, 0.9, 0.8, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.body.appendChild(renderer.domElement);
+
+    renderCube(scene, camera, renderer)
+}
+
+function renderCube(scene, camera, renderer){
+
+    const geometry = new THREE.BoxGeometry( 10, 10, 10);
+    const material = new THREE.MeshBasicMaterial( { color: 0x123456 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 25;
+
+    function render() {
+        requestAnimationFrame( render );
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render( scene, camera );
+    }
+    render();
 }
