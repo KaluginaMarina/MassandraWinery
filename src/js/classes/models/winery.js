e8da94d0e7f21593e508797073609b67d65cea37
KaluginaMarina <marin.kalugin@gmail.com>
2020-10-25 23:51:10 +0300

    added windows

--- src/js/classes/models/winery.js
+++ src/js/classes/models/winery.js
@@ -1,14 +1,13 @@
 import {
     Group,
-    BoxGeometry, MeshBasicMaterial, Mesh, ShadowMaterial, MeshStandardMaterial
+    BoxGeometry, MeshBasicMaterial, Mesh, ShadowMaterial, MeshStandardMaterial, Geometry
 } from "three";
 import {CSG} from "three-csg-ts";
 
-const FACADE_COLOR = 0xe0cfb1;
-const LOGIC_CUBE_SIZE = 3;
+let FACADE_COLOR = 0xe0cfb1;
+let LOGIC_CUBE_SIZE = 3;
 
 export default class Winery {
-
     constructor() {
         this.winery = this.createWinery();
     }
@@ -21,67 +20,76 @@ export default class Winery {
     }
 
     createFacade() {
-        const facadeMaterial = new MeshStandardMaterial({
+        let facadeMaterial = new MeshStandardMaterial({
             color: FACADE_COLOR,
             reflectivity: 0.8
         });
 
-        const group = new Group();
+        let group = new Group();
 
         // create main wall
-        const geometry = new BoxGeometry(26 * LOGIC_CUBE_SIZE, LOGIC_CUBE_SIZE, 3 * LOGIC_CUBE_SIZE);
-        const mainWall = new Mesh(geometry);
+        let mainWallGeometry = new BoxGeometry(26 * LOGIC_CUBE_SIZE, LOGIC_CUBE_SIZE, 3 * LOGIC_CUBE_SIZE);
+        let mainWall = new Mesh(mainWallGeometry);
         mainWall.updateMatrix();
 
         // inner box
-        const cutgeo = mainWall.clone();
+        let cutgeo = mainWall.clone();
         cutgeo.scale.multiplyScalar(0.95);
         cutgeo.updateMatrix();
 
         // cut inner box
-        var cube_bsp = CSG.fromMesh(mainWall);
-        var subtract_bsp = CSG.fromMesh(cutgeo);
-        const emptyCube = cube_bsp.subtract(subtract_bsp);
+        let cube_bsp = CSG.fromMesh(mainWall);
+        let subtract_bsp = CSG.fromMesh(cutgeo);
+        let emptyCube = cube_bsp.subtract(subtract_bsp);
 
-        // window box
-        const windowGeometry = new BoxGeometry(0.3 * LOGIC_CUBE_SIZE, 0.6 * LOGIC_CUBE_SIZE, 3);
-        const windowMash = new Mesh(windowGeometry);
-        windowMash.updateMatrix();
-        windowMash.position.set(10,0,4);
+        // windows box
+        let windowGeometry = new BoxGeometry(0.3 * LOGIC_CUBE_SIZE, 0.6 * LOGIC_CUBE_SIZE, 40);
 
-        // group.add(mainWall.clone());
-        // group.add(windowMash);
-
-
+        // for (var i = 0; i < 19; ++i){
+        //     let windowMash = new Mesh(windowGeometry.clone());
+        //     windowMash.position.set(-9 * LOGIC_CUBE_SIZE + LOGIC_CUBE_SIZE * i, 0, -40 + 4.5);
+        //     windowMash.updateMatrix();
+        //
+        //     subtract_bsp = CSG.fromMesh(windowMash);
+        //     emptyCube = emptyCube.subtract(subtract_bsp);
+        // }
 
         // cut windows box
-        subtract_bsp = CSG.fromMesh(windowMash);
-        var cubeWithWind = emptyCube.subtract(subtract_bsp);
+        let windowMash = new Mesh(windowGeometry);
+        windowMash.position.set(0, 0, -40 + 4.5);
+        windowMash.updateMatrix();
 
-        const mainWallMesh = CSG.toMesh(cubeWithWind, mainWall.matrix);
+        subtract_bsp = CSG.fromMesh(windowMash);
+        let cubeWithWind = emptyCube.subtract(subtract_bsp);
+
+        let mainWallMesh = CSG.toMesh(cubeWithWind, mainWall.matrix);
         mainWallMesh.material = facadeMaterial;
         mainWallMesh.position.set(0, 0, -40);
         mainWallMesh.castShadow = true;
         mainWallMesh.receiveShadow = true;
 
+        for (var i = 0; i < 19; ++i) {
+            let window = this.createWindow(-9 * LOGIC_CUBE_SIZE + LOGIC_CUBE_SIZE * i, 0, -40 + 4.5);
+            group.add(window.clone());
+        }
         group.add(mainWallMesh);
 
-        const geometry1 = new BoxGeometry(3 * LOGIC_CUBE_SIZE, LOGIC_CUBE_SIZE, 11 * LOGIC_CUBE_SIZE);
-        const leftWall = new Mesh(geometry1, facadeMaterial.clone());
+        let geometry1 = new BoxGeometry(3 * LOGIC_CUBE_SIZE, LOGIC_CUBE_SIZE, 11 * LOGIC_CUBE_SIZE);
+        let leftWall = new Mesh(geometry1, facadeMaterial.clone());
         leftWall.position.set(-35, 0, -28);
         leftWall.castShadow = true;
         leftWall.receiveShadow = true;
         group.add(leftWall);
 
-        const rightWall = new Mesh(geometry1, facadeMaterial.clone());
+        let rightWall = new Mesh(geometry1, facadeMaterial.clone());
         rightWall.position.set(35, 0, -28);
         rightWall.castShadow = true;
         rightWall.receiveShadow = true;
         group.add(rightWall);
 
-        const geometry2 = new BoxGeometry(3 * LOGIC_CUBE_SIZE, 4 * LOGIC_CUBE_SIZE, 3 * LOGIC_CUBE_SIZE);
-        const tower = new Mesh(geometry2, facadeMaterial.clone());
-        tower.position.set(0, 6, -40);
+        let geometry2 = new BoxGeometry(3 * LOGIC_CUBE_SIZE, 4 * LOGIC_CUBE_SIZE, 3 * LOGIC_CUBE_SIZE);
+        let tower = new Mesh(geometry2, facadeMaterial.clone());
+        tower.position.set(0, 7, -40);
         tower.castShadow = true;
         tower.receiveShadow = true;
         group.add(tower);
@@ -90,19 +98,17 @@ export default class Winery {
     }
 
     createWindow(x, y, z) {
-        var group = new Group();
 
-        const windowGeometry = new BoxGeometry(0.3 * LOGIC_CUBE_SIZE, 0.6 * LOGIC_CUBE_SIZE, 0.1);
-        const windowMaterial = new MeshBasicMaterial({
+        let windowGeometry = new BoxGeometry(0.3 * LOGIC_CUBE_SIZE, 0.6 * LOGIC_CUBE_SIZE, 0.1);
+        let windowMaterial = new MeshBasicMaterial({
             color: 0xffffff,
             transparent: true,
             wireframe: false,
             opacity: 0.4,
         });
-        const window = new Mesh(windowGeometry, windowMaterial);
+        let window = new Mesh(windowGeometry, windowMaterial);
         window.position.set(x, y, z);
-        group.add(window);
 
-        return group;
+        return window;
     }
 }
